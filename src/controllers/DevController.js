835a09278441c3ae42713a4b155e7f6dc95d6a34
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-01-17 12:11:42 -0300

    Feature: Real time

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = { 
   async index(request, response) {
@@ -34,6 +35,15 @@ module.exports = {
       techs: techsArray,
       location,
     })
+
+    // connection filter around 10km and common techs
+    const sendSocketMessageTo = findConnections(
+      { latitude, longitude },
+      techsArray,
+    )
+
+    sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
   }
 
     return response.json(dev);
@@ -46,4 +56,4 @@ module.exports = {
   // async destroy() {
 
   // },
-}
+};
